a67ff5a381bad5afc840c668de565b380eebaeb9
bosh-crypto <gatikwrking@gmail.com>
2026-07-01 17:02:23 +0530

    TESTlogAdded

--- 3_try_api-New--main/route/login.js
+++ 3_try_api-New--main/route/login.js
@@ -75,6 +75,7 @@ router.post("/signUp", async (req, res) => {
 
 router.get("/test", (req, res) => {
   res.status(200).json({ message: "API is working!" });
+  console.log("[TMS] Test route accessed successfully.");
 });
 
 // ---------------------------------------------------------
